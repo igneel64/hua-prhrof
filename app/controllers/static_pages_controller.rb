d8c9a333a66775a8ce9167f63d06e82b49412907
p perl <p.perlepes@gmail.com>
2016-01-13 18:35:36 +0000

    Fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,23 @@ class StaticPagesController < ApplicationController
   
   def page1
   end
+  
+  def page2
+  end
+  def page3
+  end
+  def page4
+  end
+  def page5
+  end
+  def page6
+  end
+  def page7
+  end
+  def page8
+  end
+  def page9
+  end
+  def page10
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   get 'static_pages/index'
   get 'static_pages/page1'
+  get 'static_pages/page2'
+  get 'static_pages/page3'
+  get 'static_pages/page4'
+  get 'static_pages/page5'
+  get 'static_pages/page6'
+  get 'static_pages/page7'
+  get 'static_pages/page8'
+  get 'static_pages/page9'
+  get 'static_pages/page10'
   
   root 'static_pages#index'
 
